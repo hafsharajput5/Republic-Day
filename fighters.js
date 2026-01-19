bd4b52e9971aed0a902eed0e328e97b3a75d7d34
Hafsha Rajput <khalidchou9756@gmail.com>
2026-01-19 19:16:41 +0530

    Republic Day

---
+++ fighters.js
@@ -0,0 +1,11 @@
+function jaiHind() {
+  alert("Jai Hind 🇮🇳 Happy Republic Day");
+}
+
+function check(ans) {
+  if (ans === "1950") {
+    document.getElementById("result").innerHTML = "✅ Correct Answer";
+  } else {
+    document.getElementById("result").innerHTML = "❌ Wrong Answer";
+  }
+}
